73032a19c54286b6103ba3d733b2ec706457eb18
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2024-01-09 23:36:58 +0530

    fix: menu icon display in navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -13,11 +13,11 @@ export default function Navbar() {
             <div className="mx-auto lg:w-4/5 my-6 md:flex justify-between items-baseline">
                 <div className='flex justify-between items-center'>
                     <Link href='/' style={{ "fontFamily": "'Rubik Lines', system- ui" }} className="px-4 py-4 cursor-pointer hover:text-neutral-200 text-2xl lg:text-2xl tracking-wider">sockalingam</Link>
-                    <div onClick={toggleNavbar} className='px-4 py-4'>
-                        {navbarOpen && <svg className="md:hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <div onClick={toggleNavbar} className='md:hidden px-4 py-4'>
+                        {navbarOpen && <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>}
-                        {!navbarOpen && <svg className="md:hidden block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                        {!navbarOpen && <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>}
                     </div>
@@ -26,7 +26,7 @@ export default function Navbar() {
                     <Link href='/' className="px-4 py-4 cursor-pointer hover:text-neutral-200 font-medium">home</Link>
                     <Link href="/experience" className="px-4 py-4 cursor-pointer hover:text-neutral-200 font-medium">experience</Link>
                     <Link href='/' className="px-4 py-4 cursor-pointer hover:text-neutral-200 font-medium">projects</Link>
-                    <Link href='/' className="px-4 py-4 cursor-pointer hover:text-neutral-200 font-medium">guestbook</Link>
+                    <Link href='/guestbook' className="px-4 py-4 cursor-pointer hover:text-neutral-200 font-medium">guestbook</Link>
 
                 </div>
             </div >
