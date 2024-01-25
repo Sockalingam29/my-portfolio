7468636d031cd761a05aca63a04c927b1bcbc998
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2024-01-25 23:40:51 +0530

    fix: navbar menu close

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className="sticky z-10 top-0 shadow-sm shadow-[#1b1b1b] backdrop-blur-3xl">
             <div className="mx-auto lg:w-4/5 my-6 md:flex justify-between items-baseline">
                 <div className='flex justify-between items-center'>
-                    <Link href='/' style={{ "fontFamily": "'Rubik Lines', system- ui" }} className="px-4 py-4 cursor-pointer hover:text-neutral-200 text-2xl tracking-wider">sockalingam</Link>
+                    <Link onClick={navbarOpen && toggleNavbar} href='/' style={{ "fontFamily": "'Rubik Lines', system- ui" }} className="px-4 py-4 cursor-pointer hover:text-neutral-200 text-2xl tracking-wider">sockalingam</Link>
                     <div onClick={toggleNavbar} className='md:hidden px-4 py-4'>
                         {navbarOpen && <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
