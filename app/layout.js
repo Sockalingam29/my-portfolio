42e20799ff648417cfb026e9f6e1c2cee98a16c9
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2024-01-26 00:03:33 +0530

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar'
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
