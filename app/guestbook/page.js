65261593fae6b98417e9cf4a067e3569ddf23b47
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2024-01-28 00:26:04 +0530

    add description to guestbook

--- app/guestbook/page.js
+++ app/guestbook/page.js
@@ -8,7 +8,8 @@ export default async function Home() {
     // console.log(session);
     return (
         <div className="px-4 my-16 mx-auto lg:w-4/5">
-            <h1 className="text-3xl font-medium mb-14">sign my guestbook</h1>
+            <h1 className="text-3xl font-medium">sign my guestbook</h1>
+            <p className="font-extralight text=sm mb-14 mt-4">Write something for the future visitors and me!</p>
             <GuestBookContents />
         </div>
     );
